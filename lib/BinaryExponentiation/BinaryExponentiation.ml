c650d024c697e25a4b784cdb721f311127fa7253
Vedant <vedantpandey46@gmail.com>
2024-11-10 11:11:40 +0530

    refactor(BinaryExponentiation): Refactor tests

--- lib/BinaryExponentiation/BinaryExponentiation.ml
+++ lib/BinaryExponentiation/BinaryExponentiation.ml
@@ -1,47 +1,60 @@
 open! Core
+open Z
+open Z.Compare
+
+let z = Z.of_int
+let two = z 2
+let three = z 3
+let four = z 4
+let five = z 5
+let six = z 6
+let seven = z 7
+let eight = z 8
+let nine = z 9
 
 module BinPow = struct
-  open Z
-  open Z.Compare
-
-  let rec non_bin_pow_rec_tco ?(acc = Z.of_int 1) (base : Z.t) (exp : Z.t) : Z.t =
-    match exp with
-    | exp when exp = Z.of_int 0 -> acc
-    | exp -> non_bin_pow_rec_tco ~acc:(base * acc) base (Z.pred exp)
+  let non_bin_pow_rec_tco (base : Z.t) (exp : Z.t) : Z.t =
+    let rec aux ?(acc = one) (base : Z.t) (exp : Z.t) : Z.t =
+      match exp with
+      | exp when exp = zero -> acc
+      | exp -> aux ~acc:(base * acc) base (Z.pred exp)
+    in
+    aux base exp
   ;;
 
-  let[@warning "-27"] rec bin_pow_rec ?(acc = Z.of_int 1) (base : Z.t) (exp : Z.t) : Z.t =
-    let open Z in
+  let rec bin_pow_rec (base : Z.t) (exp : Z.t) : Z.t =
     match exp with
-    | exp when exp = Z.of_int 0 -> Z.of_int 1
-    | exp when exp = Z.of_int 1 -> base
+    | exp when exp = zero -> one
+    | exp when exp = one -> base
     | exp ->
-      let res = bin_pow_rec base (exp / Z.of_int 2) in
-      (match exp land Z.of_int 1 with
-       | exp when Z.equal exp (Z.of_int 1) -> res * res * base
+      let res = bin_pow_rec base (exp / two) in
+      (match exp land one with
+       | exp when Z.equal exp one -> res * res * base
        | _ -> res * res)
   ;;
 
-  let rec bin_pow_rec_tco ?(acc = Z.of_int 1) (base : Z.t) (exp : Z.t) : Z.t =
-    let open Z.Compare in
-    match exp with
-    | exp when exp = Z.of_int 0 -> Z.of_int 1
-    | exp when exp = Z.of_int 1 -> base * acc
-    | exp ->
-      (match exp land Z.of_int 1 with
-       | rem when rem = Z.of_int 1 ->
-         bin_pow_rec_tco ~acc:(acc * base) (base * base) (exp / Z.of_int 2)
-       | _ -> bin_pow_rec_tco ~acc (base * base) (exp / Z.of_int 2))
+  let bin_pow_rec_tco (base : Z.t) (exp : Z.t) : Z.t =
+    let rec aux ?(acc = one) (base : Z.t) (exp : Z.t) : Z.t =
+      let open Z.Compare in
+      match exp with
+      | exp when exp = zero -> one
+      | exp when exp = one -> base * acc
+      | exp ->
+        (match exp land one with
+         | rem when rem = one -> aux ~acc:(acc * base) (base * base) (exp / two)
+         | _ -> aux ~acc (base * base) (exp / two))
+    in
+    aux base exp
   ;;
 
-  let[@warning "-27"] bin_pow_loop ?(acc = Z.of_int 1) base exp =
-    let res = ref (Z.of_int 1) in
+  let bin_pow_loop base exp =
+    let res = ref one in
     let exp_ref = ref exp in
     let base_ref = ref base in
-    while !exp_ref > Z.of_int 0 do
-      if !exp_ref land Z.of_int 1 = Z.of_int 1 then res := !res * !base_ref;
+    while !exp_ref > zero do
+      if !exp_ref land one = one then res := !res * !base_ref;
       base_ref := !base_ref * !base_ref;
-      exp_ref := !exp_ref / Z.of_int 2
+      exp_ref := !exp_ref / two
     done;
     !res
   ;;
@@ -49,12 +62,13 @@ end
 
 module BinPowBench = struct
   open Core_bench
+  open BinPow
 
   let bin_pow_test_suite =
-    [ "Bin exp pow", BinPow.bin_pow_rec
-    ; "Non bin exp pow", BinPow.non_bin_pow_rec_tco
-    ; "Bin pow rec TCO", BinPow.bin_pow_rec_tco
-    ; "Bin pow imperative", BinPow.bin_pow_loop
+    [ "Bin exp pow", bin_pow_rec
+    ; "Non bin exp pow", non_bin_pow_rec_tco
+    ; "Bin pow rec TCO", bin_pow_rec_tco
+    ; "Bin pow imperative", bin_pow_loop
     ]
   ;;
 
@@ -67,41 +81,28 @@ module BinPowBench = struct
          bin_pow_test_suite
          |> List.map ~f:(fun (name, func) ->
            Bench.Test.create ~name:[%string "%{name}-%{iter#Int}"] (fun () ->
-             ignore (func (Z.of_int 2) (Z.of_int iter)))))
+             ignore (func two (z iter)))))
        |> List.fold ~init:[] ~f:(fun acc elem -> List.append elem acc))
   ;;
 end
 
 module BinPowTests = struct
-  open Z.Compare
   open BinPow
 
-  let%test "non_bin_pow_rec_tco 0" =
-    non_bin_pow_rec_tco (Z.of_int 2) (Z.of_int 0) = Z.of_int 1
-  ;;
-
-  let%test "non_bin_pow_rec_tco 1" =
-    non_bin_pow_rec_tco (Z.of_int 2) (Z.of_int 1) = Z.of_int 2
-  ;;
-
-  let%test "non_bin_pow_rec_tco 2" =
-    non_bin_pow_rec_tco (Z.of_int 2) (Z.of_int 2) = Z.of_int 4
-  ;;
-
-  let%test "non_bin_pow_rec_tco 3" =
-    non_bin_pow_rec_tco (Z.of_int 2) (Z.of_int 3) = Z.of_int 8
-  ;;
-
-  let%test "bin_pow_rec 0" = bin_pow_rec (Z.of_int 2) (Z.of_int 0) = Z.of_int 1
-  let%test "bin_pow_rec 1" = bin_pow_rec (Z.of_int 2) (Z.of_int 1) = Z.of_int 2
-  let%test "bin_pow_rec 2" = bin_pow_rec (Z.of_int 2) (Z.of_int 2) = Z.of_int 4
-  let%test "bin_pow_rec 3" = bin_pow_rec (Z.of_int 2) (Z.of_int 3) = Z.of_int 8
-  let%test "bin_pow_rec_tco 0" = bin_pow_rec_tco (Z.of_int 2) (Z.of_int 0) = Z.of_int 1
-  let%test "bin_pow_rec_tco 1" = bin_pow_rec_tco (Z.of_int 2) (Z.of_int 1) = Z.of_int 2
-  let%test "bin_pow_rec_tco 2" = bin_pow_rec_tco (Z.of_int 2) (Z.of_int 2) = Z.of_int 4
-  let%test "bin_pow_rec_tco 3" = bin_pow_rec_tco (Z.of_int 2) (Z.of_int 3) = Z.of_int 8
-  let%test "bin_pow_loop 0" = bin_pow_loop (Z.of_int 2) (Z.of_int 0) = Z.of_int 1
-  let%test "bin_pow_loop 1" = bin_pow_loop (Z.of_int 2) (Z.of_int 1) = Z.of_int 2
-  let%test "bin_pow_loop 2" = bin_pow_loop (Z.of_int 2) (Z.of_int 2) = Z.of_int 4
-  let%test "bin_pow_loop 3" = bin_pow_loop (Z.of_int 2) (Z.of_int 3) = Z.of_int 8
+  let%test "non_bin_pow_rec_tco 0" = non_bin_pow_rec_tco two zero = one
+  let%test "non_bin_pow_rec_tco 1" = non_bin_pow_rec_tco two one = two
+  let%test "non_bin_pow_rec_tco 2" = non_bin_pow_rec_tco two two = four
+  let%test "non_bin_pow_rec_tco 3" = non_bin_pow_rec_tco two three = eight
+  let%test "bin_pow_rec 0" = bin_pow_rec two zero = one
+  let%test "bin_pow_rec 1" = bin_pow_rec two one = two
+  let%test "bin_pow_rec 2" = bin_pow_rec two two = four
+  let%test "bin_pow_rec 3" = bin_pow_rec two three = eight
+  let%test "bin_pow_rec_tco 0" = bin_pow_rec_tco two zero = one
+  let%test "bin_pow_rec_tco 1" = bin_pow_rec_tco two one = two
+  let%test "bin_pow_rec_tco 2" = bin_pow_rec_tco two two = four
+  let%test "bin_pow_rec_tco 3" = bin_pow_rec_tco two three = eight
+  let%test "bin_pow_loop 0" = bin_pow_loop two zero = one
+  let%test "bin_pow_loop 1" = bin_pow_loop two one = two
+  let%test "bin_pow_loop 2" = bin_pow_loop two two = four
+  let%test "bin_pow_loop 3" = bin_pow_loop two three = eight
 end
